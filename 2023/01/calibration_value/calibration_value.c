6dd3ec5f670b0cc49418f6c8232c0c99056a7b3c
BloatedMonke <pjlkkia@gmail.com>
2023-12-01 15:48:01 +0000

    ADVENT OF CODE 2023!

---
+++ 2023/01/calibration_value/calibration_value.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+typedef enum bool {
+    false = 0,
+    true
+}
+bool;
+
+int main(void){
+    const char *input_file = "input.txt";
+    FILE *input = fopen(input_file, "r");
+    
+    char c;
+    int sum = 0;
+    bool you_are_the_first = true;
+    while (fread(&c, sizeof(char), 1, input)){
+        int first_num, last_num, num;
+
+        if (isdigit(c))
+            num = c - '0';
+        if (isdigit(c) && you_are_the_first){
+            first_num = c - '0';
+            you_are_the_first = false;
+        }
+        if (c == '\n'){
+            last_num = num;
+            sum += first_num * 10 + last_num;
+            you_are_the_first = true;
+        }
+    }
+    
+    printf("sum of all calibration values: %d\n", sum);
+    return 0;
+}
