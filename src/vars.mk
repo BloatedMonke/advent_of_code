23f79cb4e7b64375cc630fe0b02c8ea4f037abfc
BloatedMonke <pjlkkia@gmail.com>
2023-12-03 00:44:42 +0000

    added debug build

--- src/vars.mk
+++ src/vars.mk
@@ -2,4 +2,7 @@ VPATH = ../../include ../../dyn_string
 CC = gcc
 INCLUDES = -I../../include
 CFLAGS = $(INCLUDES) -Wall -Wpedantic -Wextra
+DBGFLAGS = $(CFLAGS) -g3
+SANITIZE = -fsanitize=address
+DBGFLAGS += $(SANITIZE)
  
