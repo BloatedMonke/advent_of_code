7047ad00bc897ddbab86958d5101224c84907c54
BloatedMonke <pjlkkia@gmail.com>
2023-12-06 00:27:58 +0000

    ensure no optimisations for debug

--- src/vars.mk
+++ src/vars.mk
@@ -2,7 +2,7 @@ VPATH = ../../include ../../dyn_string
 CC = gcc
 INCLUDES = -I../../include
 CFLAGS = $(INCLUDES) -Wall -Wpedantic -Wextra
-DBGFLAGS = $(CFLAGS) -g3
+DBGFLAGS = $(CFLAGS) -g3 -O0
 SANITIZE = -fsanitize=address
 DBGFLAGS += $(SANITIZE)
  
