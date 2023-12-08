bed52c1215d734c633e28cc0470c2063c2acb7da
BloatedMonke <pjlkkia@gmail.com>
2023-12-08 15:28:44 +0000

    moved max to common utilities

--- src/common/utility.h
+++ src/common/utility.h
@@ -1,6 +1,8 @@
 #ifndef _UTILITY_H_
 #define _UTILITY_H_
 
+#define max(a, b) ((a) > (b) ? (a): (b))
+
 int strtoint(const char * const s, int len);
 int str_num_width(const char * const s, int max_step);
 
