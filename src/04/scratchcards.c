7d629de290cb6f4bc28885d527313d4f94e168a2
BloatedMonke <pjlkkia@gmail.com>
2023-12-10 00:31:29 +0000

    merged day part1 and part2 into one function

--- src/04/scratchcards.c
+++ src/04/scratchcards.c
@@ -6,9 +6,16 @@
 #ifndef FILE_LENGTH
 #define FILE_LENGTH 198
 #endif
+#ifndef MAX_LEN
+#define MAX_LEN 25
+#endif
 
-uint32_t solve(FILE *file, int my_len);
-uint32_t solve2(FILE *file, int my_len);
+struct solution {
+    uint32_t part1;
+    uint32_t part2;
+};
+
+struct solution solve(FILE *file);
 
 int main(int argc, const char *argv[])
 {
@@ -23,105 +30,44 @@ int main(int argc, const char *argv[])
         return 2;
     }
 
-    /** get len of my tic and win tic */
-    DynString *str = NULL;
-    dyn_string_get_file_line(&str, file);
-    const char * const s = get_str_arr(str);
-    int i, my_len = 0;
-    for (i = 8; s[i] != '|'; ++i);;
-
-    for (int j = i; j < get_lengthof(str) ; ++j){
-        if (isdigit(s[j])){
-            int num_width = str_num_width(s + j, get_lengthof(str) - j);
-            ++my_len;
-            j += num_width;
-        }
-    }
-    rewind(file);
-
-    uint32_t points = solve(file, my_len);
-    rewind(file);
-    uint32_t points2 = solve2(file, my_len);
+    struct solution points = solve(file);
     fclose(file);
 
     printf("POINTS::\n");
-    printf("Part 1: %d\n", points);
-    printf("Part 2: %d\n", points2);
+    printf("Part 1: %d\n", points.part1);
+    printf("Part 2: %d\n", points.part2);
 
     return 0;
 }
 
-uint32_t solve(FILE *file, int my_len){
-    uint32_t my_set[my_len];
-    int sum = 0;
+struct solution solve(FILE *file){
     int n = 0;
-
-    DynString *str = NULL;
-    while (dyn_string_get_file_line(&str, file)){
-        
-        uint32_t win_hashmap[100] = {0};
-        const char * const s = get_str_arr(str);
-        int i = 4;
-        while (s[i] != ':') ++i;
-        
-        int xcount = 0;
-        for (; s[i] != '|'; ++i){
-            if (isdigit(s[i])){
-                int num_width = str_num_width(s + i, get_lengthof(str) - i);
-                int x = strtoint(s + i, num_width);
-                win_hashmap[x] = x;
-                i += num_width;
-            }
-        }
-        for (int j = i + 2; j < get_lengthof(str); ++j){
-            if (isdigit(s[j])){
-                int num_width = str_num_width(s + j, get_lengthof(str) - j);
-                int x = strtoint(s + j, num_width);
-                my_set[xcount] = x;
-                ++xcount;
-                j += num_width;
-           }
-       }
-       for (int k = 0; k < my_len; ++k){
-           if (win_hashmap[my_set[k]])
-               ++n;
-       }
-       sum += n ? 1 << (n - 1): 0;
-       n = 0;
-       dyn_string_kill(str);
-    }
-    dyn_string_kill(str);
+    uint32_t my_set[MAX_LEN];
     
-    return sum;
-}
-
-/* part 1 hand holds part 2 so this is largely the same */
-uint32_t solve2(FILE *file, int my_len){
-    uint32_t card_hashmap[FILE_LENGTH] = {0};
-    uint32_t my_set[my_len];
-    int n  = 0;
+    int sum1 = 0;
+    
+    uint32_t card_hashmap[FILE_LENGTH] = {0};    
     int card = 0;
-    int sum  = 0;
     int cc = 0;
-
+    
     DynString *str = NULL;
     while (dyn_string_get_file_line(&str, file)){
         ++card_hashmap[cc++];
 
         uint32_t win_hashmap[100] = {0};
         const char * const s = get_str_arr(str);
+        
         int i = 4;
         while (s[i] != ':') ++i;
         
-        int xcount = 0;
-        for (; s[i] != '|'; ++i){
+        for (; s[i] != '|'; ++i)
             if (isdigit(s[i])){
                 int num_width = str_num_width(s + i, get_lengthof(str) - i);
                 int x = strtoint(s + i, num_width);
                 win_hashmap[x] = x;
                 i += num_width;
             }
-        }
+        int xcount = 0;
         for (int j = i + 2; j < get_lengthof(str); ++j){
             if (isdigit(s[j])){
                 int num_width = str_num_width(s + j, get_lengthof(str) - j);
@@ -130,23 +76,24 @@ uint32_t solve2(FILE *file, int my_len){
                 ++xcount;
                 j += num_width;
            }
-       }
-       for (int k = 0; k < my_len; ++k){
+        }
+        for (int k = 0; k < xcount; ++k){
            if (win_hashmap[my_set[k]])
                ++n;
-       }
-
-       for (int q = card + 1; q <= (card + n); ++q)
-           card_hashmap[q] += card_hashmap[card];
-
-       n = 0;
-       dyn_string_kill(str);
-       ++card;
+        }
+        sum1 += n ? 1 << (n - 1): 0;
+       
+        for (int q = card + 1; q <= (card + n); ++q)
+            card_hashmap[q] += card_hashmap[card];
+        ++card;
+        n = 0;
+        dyn_string_kill(str);
     }
     dyn_string_kill(str);
-    
-    for (int i = 0; i < card; ++i)
-        sum += card_hashmap[i];
 
-    return sum;
+    int sum2 = 0;
+    for (int i = 0; i < card; ++i)
+        sum2 += card_hashmap[i];
+    
+    return (struct solution){.part1 = sum1, .part2 = sum2};
 }
